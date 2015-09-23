aa5d1241f520989f400fef7051f58af2218680a8
Breena Theseira <breenatheseira@yahoo.com>
2015-09-23 18:58:15 +0800

    Updated the index page to use the new reporting method and removed the old reporting method from the model

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,5 +1,2 @@
 class Robot < ActiveRecord::Base
-	def reported_position(robot)
-		robot.x_coordinate.blank? ? "Robot #{robot.name} is not placed on the table yet." : "#{robot.x_coordinate}, #{robot.y_coordinate}, #{robot.position}"
-	end
 end
