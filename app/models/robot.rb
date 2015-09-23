332a7a596b8e6d1161ca1c1001c02c403495f138
Breena Theseira <breenatheseira@yahoo.com>
2015-09-23 16:10:45 +0800

    Edited Index to show all current robots and their positions.

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,2 +1,5 @@
 class Robot < ActiveRecord::Base
+	def reported_position(robot)
+		robot.x_coordinate.blank? ? "Robot #{robot.name} is not placed on the table yet." : "#{robot.x_coordinate}, #{robot.y_coordinate}, #{robot.position}"
+	end
 end
