234de1d7ce25b76a42fc292c550642601df8e41a
Breena Theseira <breenatheseira@yahoo.com>
2015-09-23 16:03:17 +0800

    Able to create robot successfully

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -69,6 +69,6 @@ class RobotsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def robot_params
-      params[:robot]
+      params[:robot].permit(:name, :x_coordinate, :y_coordinate, :position, :command)
     end
 end
